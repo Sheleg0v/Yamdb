5fda78db34ef620fa82da7c836d7dad6d076df11
Иван Шелегов <ivan.shelegov@yandex.ru>
2023-04-28 14:52:15 +0300

    v3.8

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -5,7 +5,7 @@ from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
                     ReviewViewSet, TitleViewSet)
 
 router_v1 = DefaultRouter()
-router_v1.register('titles', TitleViewSet, basename='titles')
+# router_v1.register('titles', TitleViewSet, basename='titles')
 router_v1.register('genres', GenreViewSet, basename='genres')
 router_v1.register('categories', CategoryViewSet, basename='categories')
 router_v1.register(
